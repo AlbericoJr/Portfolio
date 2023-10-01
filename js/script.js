504356708ddf57f8c859c1f047ece3fec86ed2ae
Albérico Junior <40373171+AlbericoJr@users.noreply.github.com>
2023-10-01 14:47:09 -0300

    Portfólio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+/*Menu Mobilty */
 
-menu.onclick = () => {
-  menu.classList.toggle('bx-x')
-  navbar.classList.toggle('open')
+const btnMobile = document.getElementById("btn-mobile")
+
+function toggleMenu(e) {
+  if(e.type === "touchstart") e.preventDefaul()
+  const nav = document.getElementById("nav")
+  nav.classList.toggle("active")
+
+  const active = nav.classList.contains("active")
+
+  e.currentTarget.setAttribute("aria-expamded", active)
+
+  if(active){
+    e.currentTarget.setAttribute("aria-label", "Fechar Menu")
+  }else{
+    e.currentTarget.setAttribute("aria-label", "Abrir Menu")
+  }
 }
+
+btnMobile.addEventListener("click", toggleMenu)
+btnMobile.addEventListener("touchstart", toggleMenu)
